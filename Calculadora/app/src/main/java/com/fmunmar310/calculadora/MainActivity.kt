82b69576154f7be7db400b645e0990b2b4a57da9
GaelCobain <fmunmar310@g.educaand.es>
2023-10-10 21:29:03 +0200

    Falta añadir comentarios en las operaciones

--- Calculadora/app/src/main/java/com/fmunmar310/calculadora/MainActivity.kt
+++ Calculadora/app/src/main/java/com/fmunmar310/calculadora/MainActivity.kt
@@ -6,60 +6,112 @@ import android.view.View
 import android.widget.Button
 import android.widget.TextView
 
+/**
+ * Main activity de nuestra calculadora
+ */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val textView = findViewById<TextView>(R.id.textView)
+
+        /**
+         * @param result variable que vamos a mostrar por el textview
+         * @param operador variable que nos dará la operación a realizar
+         * @param num1 variable de número 1 de la operación
+         * @param num2 variable de número 2 de la operación
+         */
         var result = " "
         var operador=""
         var num1 = 0.0
         var num2 = 0.0
+
+        /**
+         * @param variable que usaremos para crear nuestro cálculo
+         * @see Calculo para más información
+         */
         var calculo = Calculo(num1,num2,operador)
+
+        /**
+         * botón del número 1
+         */
         val button1 = findViewById<Button>(R.id.num1)
         button1.setOnClickListener(View.OnClickListener {
             result+="1"
             textView.text = result})
+        /**
+         * botón del número 2
+         */
         val button2 = findViewById<Button>(R.id.num2)
         button2.setOnClickListener(View.OnClickListener {
             result+="2"
             textView.text = result})
+        /**
+         * botón del número 3
+         */
         val button3 = findViewById<Button>(R.id.num3)
         button3.setOnClickListener(View.OnClickListener {
             result+="3"
             textView.text = result})
+        /**
+         * botón del número 4
+         */
         val button4 = findViewById<Button>(R.id.num4)
         button4.setOnClickListener(View.OnClickListener {
             result+="4"
             textView.text = result})
+        /**
+         * botón del número 5
+         */
         val button5 = findViewById<Button>(R.id.num5)
         button5.setOnClickListener(View.OnClickListener {
             result+="5"
             textView.text = result})
         val button6 = findViewById<Button>(R.id.num6)
+        /**
+         * botón del número 6
+         */
         button6.setOnClickListener(View.OnClickListener {
             result+="6"
             textView.text = result})
+        /**
+         * botón del número 7
+         */
         val button7 = findViewById<Button>(R.id.num7)
         button7.setOnClickListener(View.OnClickListener {
             result+="7"
             textView.text = result})
+        /**
+         * botón del número 8
+         */
         val button8 = findViewById<Button>(R.id.num8)
         button8.setOnClickListener(View.OnClickListener {
             result+="8"
             textView.text = result})
+        /**
+         * botón del número 9
+         */
         val button9 = findViewById<Button>(R.id.num9)
         button9.setOnClickListener(View.OnClickListener {
             result+="9"
             textView.text = result})
+        /**
+         * botón del número 0
+         */
         val button0 = findViewById<Button>(R.id.num0)
         button0.setOnClickListener(View.OnClickListener {
             result+="0"
             textView.text = result})
+        /**
+         * botón para añadir punto
+         */
         val buttonDot = findViewById<Button>(R.id.dot)
         buttonDot.setOnClickListener(View.OnClickListener {
             result+="."
             textView.text = result})
+        /**
+         * botón para añadir suma como operación
+         */
         val buttonPlus = findViewById<Button>(R.id.plus)
         buttonPlus.setOnClickListener(View.OnClickListener {
             if (operador == "") {
@@ -90,6 +142,9 @@ class MainActivity : AppCompatActivity() {
                 operador="+"
             }
         })
+        /**
+         * botón para añadir resta como operación
+         */
         val buttonMinus = findViewById<Button>(R.id.minus)
         buttonMinus.setOnClickListener(View.OnClickListener {
             if (operador == "") {
@@ -120,6 +175,9 @@ class MainActivity : AppCompatActivity() {
                 operador="-"
             }
         })
+        /**
+         * botón para añadir multiplicación como operación
+         */
         val buttonMult = findViewById<Button>(R.id.multiply)
         buttonMult.setOnClickListener(View.OnClickListener {
             if (operador == ""){
@@ -151,6 +209,9 @@ class MainActivity : AppCompatActivity() {
                 operador="*"
             }
         })
+        /**
+         * botón para añadir división como operación
+         */
         val buttonDiv = findViewById<Button>(R.id.divide)
         buttonDiv.setOnClickListener(View.OnClickListener {
             if(operador==""){
@@ -181,7 +242,9 @@ class MainActivity : AppCompatActivity() {
                 result=""
                 operador="/"
             }
-        })
+        })/**
+         * botón para borrar todo
+         */
         val buttonCe = findViewById<Button>(R.id.ce)
         buttonCe.setOnClickListener(View.OnClickListener {
             result = ""
@@ -189,6 +252,9 @@ class MainActivity : AppCompatActivity() {
             num2 = 0.0
             textView.text = result
         })
+        /**
+         * botón para obtener el resultado
+         */
         val buttonEqual = findViewById<Button>(R.id.equal)
         buttonEqual.setOnClickListener(View.OnClickListener {
             num2=result.toDouble()
