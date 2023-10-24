1f24696e4fcf85c2c38c68347b6af550557be2fb
GaelCobain <fmunmar310@g.educaand.es>
2023-10-24 20:12:17 +0200

    Vacio

--- Calculadora/app/src/main/java/com/fmunmar310/calculadora/MainActivity.kt
+++ Calculadora/app/src/main/java/com/fmunmar310/calculadora/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 
 /**
  * Main activity de nuestra calculadora
@@ -21,263 +22,246 @@ class MainActivity : AppCompatActivity() {
          * @param num1 variable de número 1 de la operación
          * @param num2 variable de número 2 de la operación
          */
-        var result = " "
-        var operador=""
-        var num1 = 0.0
-        var num2 = 0.0
+        var result = ""
+        var operador = ""
+
 
         /**
-         * @param variable que usaremos para crear nuestro cálculo
+         * @param calculo variable que usaremos para crear nuestro cálculo
          * @see Calculo para más información
          */
-        var calculo = Calculo(num1,num2,operador)
+        var calculo = Calculo()
 
-        /**
-         * botón del número 1
-         */
+        //botón del número 1, añade a la variable text el valor correspondiente
         val button1 = findViewById<Button>(R.id.num1)
         button1.setOnClickListener(View.OnClickListener {
-            result+="1"
-            textView.text = result})
-        /**
-         * botón del número 2
-         */
+            result += "1"
+            textView.text = result
+        })
+
+        //botón del número 2, añade a la variable text el valor correspondiente
         val button2 = findViewById<Button>(R.id.num2)
         button2.setOnClickListener(View.OnClickListener {
-            result+="2"
-            textView.text = result})
-        /**
-         * botón del número 3
-         */
+            result += "2"
+            textView.text = result
+        })
+
+        //botón del número 3, añade a la variable text el valor correspondiente
         val button3 = findViewById<Button>(R.id.num3)
         button3.setOnClickListener(View.OnClickListener {
-            result+="3"
-            textView.text = result})
-        /**
-         * botón del número 4
-         */
+            result += "3"
+            textView.text = result
+        })
+
+        //botón del número 4, añade a la variable text el valor correspondiente
         val button4 = findViewById<Button>(R.id.num4)
         button4.setOnClickListener(View.OnClickListener {
-            result+="4"
-            textView.text = result})
-        /**
-         * botón del número 5
-         */
+            result += "4"
+            textView.text = result
+        })
+
+        //botón del número 5, añade a la variable text el valor correspondiente
         val button5 = findViewById<Button>(R.id.num5)
         button5.setOnClickListener(View.OnClickListener {
-            result+="5"
-            textView.text = result})
+            result += "5"
+            textView.text = result
+        })
         val button6 = findViewById<Button>(R.id.num6)
-        /**
-         * botón del número 6
-         */
+
+        // botón del número 6, añade a la variable text el valor correspondiente
         button6.setOnClickListener(View.OnClickListener {
-            result+="6"
-            textView.text = result})
-        /**
-         * botón del número 7
-         */
+            result += "6"
+            textView.text = result
+        })
+
+        // botón del número 7, añade a la variable text el valor correspondiente
         val button7 = findViewById<Button>(R.id.num7)
         button7.setOnClickListener(View.OnClickListener {
-            result+="7"
-            textView.text = result})
-        /**
-         * botón del número 8
-         */
+            result += "7"
+            textView.text = result
+        })
+
+        // botón del número 8, añade a la variable text el valor correspondiente
         val button8 = findViewById<Button>(R.id.num8)
         button8.setOnClickListener(View.OnClickListener {
-            result+="8"
-            textView.text = result})
-        /**
-         * botón del número 9
-         */
+            result += "8"
+            textView.text = result
+        })
+
+        // botón del número 9, añade a la variable text el valor correspondiente
+
         val button9 = findViewById<Button>(R.id.num9)
         button9.setOnClickListener(View.OnClickListener {
-            result+="9"
-            textView.text = result})
-        /**
-         * botón del número 0
-         */
+            result += "9"
+            textView.text = result
+        })
+
+        // botón del número 0, añade a la variable text el valor correspondiente
         val button0 = findViewById<Button>(R.id.num0)
         button0.setOnClickListener(View.OnClickListener {
-            result+="0"
-            textView.text = result})
-        /**
-         * botón para añadir punto
-         */
+            result += "0"
+            textView.text = result
+        })
+
+        // botón para añadir punto, añade a la variable text el valor correspondiente
         val buttonDot = findViewById<Button>(R.id.dot)
         buttonDot.setOnClickListener(View.OnClickListener {
-            result+="."
-            textView.text = result})
-        /**
-         * botón para añadir suma como operación, si operador ya tiene un valor asignado realizará primero la operación designada
-         * y luego cambiará el valor de la variable operador. Si no hay un operador asignado primero se comprueba si ya hay num1 añadido,
-         * en caso negativo añade num1, en caso positivo realiza la operación y traslada el valor de la misma a num1.
-         * Al final de cada operación reseteamos el valor de num2 y de operador.
-         */
+            result += "."
+            textView.text = result
+        })
+
+          // botón para añadir suma como operación
         val buttonPlus = findViewById<Button>(R.id.plus)
         buttonPlus.setOnClickListener(View.OnClickListener {
-            if (operador == "") {
+            if (compruebaTamañoText(result)) { // si no hay ningún número añadido lanza mensaje de error
+                lanzarMensajeToast()
+            }else {
                 operador = "+"
-                if (num1 == 0.0) {
-                    num1 = result.toDouble()
-                    textView.text = result
-                    result = " "
-                } else {
-                    num2 = result.toDouble()
-                    textView.text = result
-                    calculo = Calculo(num1, num2, operador)
-                    result = calculo.operacion(operador).toString()
-                    textView.text = result
-                    num1 = result.toDouble()
-                    num2 = 0.0
-                    result = ""
-                }
-            }else{
-                num2 = result.toDouble()
+                pulsaBoton(result, operador, calculo) // llamada a la función pulsaBoton
+                // realizamos el cálculo y lo mostramos con un formato determinado añadiendo el operador a la variable result
+                result = formatear(calculo.operacion()) + operador
                 textView.text = result
-                calculo = Calculo(num1, num2, operador)
-                result = calculo.operacion(operador).toString()
-                textView.text = result
-                num1 = result.toDouble()
-                num2 = 0.0
-                result = ""
-                operador="+"
             }
+
         })
-        /**
-         * botón para añadir resta como operación, si operador ya tiene un valor asignado realizará primero la operación designada
-         * y luego cambiará el valor de la variable operador. Si no hay un operador asignado primero se comprueba si ya hay num1 añadido,
-         * en caso negativo añade num1, en caso positivo realiza la operación y traslada el valor de la misma a num1.
-         * Al final de cada operación reseteamos el valor de num2 y de operador.
-         */
+
+          // botón para añadir resta como operación
+
         val buttonMinus = findViewById<Button>(R.id.minus)
         buttonMinus.setOnClickListener(View.OnClickListener {
-            if (operador == "") {
+            if (compruebaTamañoText(result)) { // si no hay ningún número añadido lanza mensaje de error
+                lanzarMensajeToast()
+            }else {
                 operador = "-"
-                if (num1 == 0.0) {
-                    num1 = result.toDouble()
-                    textView.text = result
-                    result = " "
-                } else {
-                    num2 = result.toDouble()
-                    textView.text = result
-                    calculo = Calculo(num1, num2, operador)
-                    result = calculo.operacion(operador).toString()
-                    textView.text = result
-                    num1 = result.toDouble()
-                    num2 = 0.0
-                    result = " "
-                }
-            }else{
-                num2 = result.toDouble()
+                pulsaBoton(result, operador, calculo) // llamada a la función pulsaBoton
+                // realizamos el cálculo y lo mostramos con un formato determinado añadiendo el operador a la variable result
+                result = formatear(calculo.operacion()) + operador
                 textView.text = result
-                calculo = Calculo(num1, num2, operador)
-                result = calculo.operacion(operador).toString()
-                textView.text = result
-                num1 = result.toDouble()
-                num2 = 0.0
-                result = " "
-                operador="-"
             }
+
         })
-        /**
-         * botón para añadir multiplicación como operación, si operador ya tiene un valor asignado realizará primero la operación designada
-         * y luego cambiará el valor de la variable operador. Si no hay un operador asignado primero se comprueba si ya hay num1 añadido,
-         * en caso negativo añade num1, en caso positivo realiza la operación y traslada el valor de la misma a num1.
-         * Al final de cada operación reseteamos el valor de num2 y de operador.
-         */
+
+          // botón para añadir multiplicación como operación
+
         val buttonMult = findViewById<Button>(R.id.multiply)
         buttonMult.setOnClickListener(View.OnClickListener {
-            if (operador == ""){
+            if (compruebaTamañoText(result)) { // si no hay ningún número añadido lanza mensaje de error
+                lanzarMensajeToast()
+            }else {
                 operador = "*"
-                if (num1 == 0.0) {
-                    num1=result.toDouble()
-                    textView.text = result
-                    result=" "
-                }
-                else{
-                    num2 = result.toDouble()
-                    textView.text = result
-                    calculo = Calculo(num1,num2,operador)
-                    result=calculo.operacion(operador).toString()
-                    textView.text = result
-                    num1 = result.toDouble()
-                    num2 = 0.0
-                    result=""
-                }
-            }else{
-                num2 = result.toDouble()
+                pulsaBoton(result, operador, calculo) // llamada a la función pulsaBoton
+                // realizamos el cálculo y lo mostramos con un formato determinado añadiendo el operador a la variable result
+                result = formatear(calculo.operacion()) + operador
                 textView.text = result
-                calculo = Calculo(num1,num2,operador)
-                result=calculo.operacion(operador).toString()
-                textView.text = result
-                num1 = result.toDouble()
-                num2 = 0.0
-                result=""
-                operador="*"
             }
+
         })
-        /**
-         * botón para añadir división como operación, si operador ya tiene un valor asignado realizará primero la operación designada
-         * y luego cambiará el valor de la variable operador. Si no hay un operador asignado primero se comprueba si ya hay num1 añadido,
-         * en caso negativo añade num1, en caso positivo realiza la operación y traslada el valor de la misma a num1.
-         * Al final de cada operación reseteamos el valor de num2 y de operador.
-         */
+
+          // botón para añadir división como operación
         val buttonDiv = findViewById<Button>(R.id.divide)
         buttonDiv.setOnClickListener(View.OnClickListener {
-            if(operador==""){
-            operador = "/"
-            if (num1 == 0.0) {
-                num1=result.toDouble()
+            if (compruebaTamañoText(result)) { // si no hay ningún número añadido lanza mensaje de error
+                lanzarMensajeToast()
+            }else {
+                operador = "/"
+                pulsaBoton(result, operador, calculo) // llamada a la función pulsaBoton
+                // realizamos el cálculo y lo mostramos con un formato determinado añadiendo el operador a la variable result
+                result = formatear(calculo.operacion()) + operador
                 textView.text = result
-                result=" "
             }
-            else{
-                num2 = result.toDouble()
+        })
+
+        // botón par borrar el último caracter añadido
+        val buttonDel = findViewById<Button>(R.id.del)
+        buttonDel.setOnClickListener(View.OnClickListener {
+            if(result.length < 1){ // Si no hay nada añadido a la variable text lanza mensaje de error
+                errorBorrar()
+            }else {
+                var tempBorr = "" // variable que se usa para pasar de Double a text los atributos de la calse calculo
+                if(calculo.numUno){ // comprueba si se está añadiendo el primer número
+                    tempBorr = calculo.num1.toString() // se asigna el valor de num1 a tempBorr
+                    tempBorr = tempBorr.substring(0,tempBorr.length-1) // se elimina el último caracter añadido
+                    calculo.num1 = tempBorr.toDouble() // se reasigna el valor a num1
+                }else { // si ya ha sido añadido el primer número se realiza la misma operación con num2
+                    tempBorr = calculo.num2.toString()
+                    tempBorr = tempBorr.substring(0, tempBorr.length - 1)
+                    calculo.num2 = tempBorr.toDouble()
+                    }
+                result = result.substring(0, result.length - 1) // se elimina el valor de result
                 textView.text = result
-                calculo = Calculo(num1,num2,operador)
-                result=calculo.operacion(operador).toString()
-                textView.text = result
-                num1 = result.toDouble()
-                num2 = 0.0
-                result=""
+                if(tempBorr.isEmpty()){calculo.numUno = true}
+            //si tempBorr está vacío se indica que numUno es true
             }
-        }else{
-                num2 = result.toDouble()
-                textView.text = result
-                calculo = Calculo(num1,num2,operador)
-                result=calculo.operacion(operador).toString()
-                textView.text = result
-                num1 = result.toDouble()
-                num2 = 0.0
-                result=""
-                operador="/"
-            }
-        })/**
-         * botón para resetear los valores a su valor inicial
-         */
+        })
+
+        // botón para resetear los valores a su valor inicial
         val buttonCe = findViewById<Button>(R.id.ce)
         buttonCe.setOnClickListener(View.OnClickListener {
             result = ""
-            num1 = 0.0
-            num2 = 0.0
+            calculo.resetear()
             textView.text = result
         })
-        /**
-         * botón para obtener el resultado, se crea el objeto cálculo con los valores num1, num2 y operación y se llama al método operación del mismo.
-         * @see Calculo
-         */
+
+        // botón para obtener el resultado
+
         val buttonEqual = findViewById<Button>(R.id.equal)
         buttonEqual.setOnClickListener(View.OnClickListener {
-            num2=result.toDouble()
-            textView.text = result
-            calculo = Calculo(num1,num2,operador)
-            result = calculo.operacion(operador).toString()
-            textView.text = result
-            num1 = result.toDouble()
-            num2 = 0.0
-            result="0"
+            if (compruebaTamañoText(result)) { // si no hay ningún número añadido lanza mensaje de error
+                lanzarMensajeToast()
+            }else {
+                operador = "="
+                pulsaBoton(result, operador, calculo) // llamada a la función pulsaBoton
+                // realizamos el cálculo y lo mostramos con un formato determinado añadiendo el operador a la variable result
+                result = formatear(calculo.operacion())
+                textView.text = result
+                calculo.numUno = true
+            }
         })
     }
+
+    fun formatear(num: Double): String {
+        return String.format("%.2f", num)
+    }
+
+
+    fun pulsaBoton(cadena: String, oper: String, calculo: Calculo) {
+            var lista = listOf<String>()
+            if (oper == "=") {
+                for (i in cadena) {
+                    if (i == '+' || i == '-' || i == '*' || i == '/') {
+                        lista = cadena.split(i)
+                        calculo.num1 = lista[0].toDouble()
+                        calculo.num2 = lista[1].toDouble()
+                        calculo.op = "$i"
+                    }
+                }
+            } else {
+                if (calculo.numUno) {
+                    calculo.num1 = cadena.toDouble()
+                } else {
+                    for (i in cadena) {
+                        if (i == '+' || i == '-' || i == '*' || i == '/') {
+                            lista = cadena.split(i)
+                            calculo.num1 = lista[0].toDouble()
+                            calculo.num2 = lista[1].toDouble()
+                            calculo.op = "$i"
+                        }
+                    }
+                }
+            }
+        }
+    fun compruebaTamañoText(text: String): Boolean {
+        if (text.isEmpty()) {
+            return true
+        }
+        return false
+    }
+    fun lanzarMensajeToast() {
+        Toast.makeText(this, "Error, faltan datos o son erróneos", Toast.LENGTH_SHORT).show()
+    }
+    fun errorBorrar(){
+        Toast.makeText(this, "No se puede borrar", Toast.LENGTH_SHORT).show()
+
+    }
 }
+
